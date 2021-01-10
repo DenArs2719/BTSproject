f5935bd65b51d79424a7d5437e0ba2d1345483bc
Zagurskiy Vladyslav <magician.vz17@outlook.com>
2021-01-10 17:47:09 +0100

    added class ReviewAdapter

--- app/src/main/java/com/example/btsproject/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/btsproject/adapters/MovieAdapter.java
@@ -4,66 +4,134 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-
 import com.example.btsproject.R;
-import com.example.btsproject.data.Review;
+import com.example.btsproject.data.Movie;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class ReviewAdapter extends RecyclerView.Adapter<ReviewAdapter.ReviewViewHolder>
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder>
 {
-    private ArrayList<Review> reviews;
+    private List<Movie> movies;
+
+
+    private OnPosterClickListener onPosterClickListener;
+    private OnReachEndListener onReachEndListener;
+
+    public MovieAdapter()
+    {
+        movies = new ArrayList<>();
+    }
+
+    ///интерфейс для обработки нажатия на фильм(постер)
+    public interface OnPosterClickListener
+    {
+        void onPosterClick(int position);
+    }
+
+    ///интерфейс для подгрузки фильмом,когда пользователь долистал до конца первую старницу и т.д
+    public interface OnReachEndListener
+    {
+        void onReachEnd();
+    }
 
     @NonNull
     @Override
-    public ReviewViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+    public MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
     {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.review_item,parent,false);
-        return new ReviewViewHolder(view);
+        ///создаем наш View
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.movie_item,parent,false);
+        return new MovieViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ReviewViewHolder holder, int position)
+    public void onBindViewHolder(@NonNull MovieViewHolder holder, int position)
     {
-        ///получаем наш отзыв
-        Review review = reviews.get(position);
+        ///подгружаем данные, когда пользователь практически дошел до конца списка
+        if(movies.size()>=20 && position > movies.size() - 4 && onReachEndListener != null)
+        {
+            onReachEndListener.onReachEnd();
+        }
+        ///получаем фильм
+        Movie movie = movies.get(position);
+        ///получем картинку
+        ImageView imageView = holder.imageViewSmallPoster;
 
-        ///устанавливаем полученные данные
-        holder.textViewContent.setText(review.getContent());
-        holder.textViewAuthor.setText(review.getAuthor());
+        ///работаем с Picasso , и загружаем изображение
+        Picasso.get().load(movie.getPosterPath()).into(imageView);
     }
 
     @Override
     public int getItemCount()
     {
-        return reviews.size();
+        return movies.size();
     }
 
-    class ReviewViewHolder extends RecyclerView.ViewHolder
+    public void addMovies(List<Movie> movies)
     {
-        private TextView textViewContent;
-        private TextView textViewAuthor;
+        this.movies.addAll(movies);
 
-        public ReviewViewHolder(@NonNull View itemView)
+        ///когда добавили новые элементы,оповешаем об этом наш адаптер
+        notifyDataSetChanged();
+    }
+
+
+    public List<Movie> getMovies()
+    {
+        return movies;
+    }
+
+    public void clear()
+    {
+        this.movies.clear();
+
+        ///оповешаем адаптер о том ,что произошли изменения
+        notifyDataSetChanged();
+    }
+
+    public void setMovies(List<Movie> movies)
+    {
+        this.movies = movies;
+
+        ///когда добавили новые элементы,оповешаем об этом наш адаптер
+        notifyDataSetChanged();
+    }
+
+    public void setOnPosterClickListener(OnPosterClickListener onPosterClickListener)
+    {
+        this.onPosterClickListener = onPosterClickListener;
+    }
+
+    public void setOnReachEndListener(OnReachEndListener onReachEndListener)
+    {
+        this.onReachEndListener = onReachEndListener;
+    }
+
+    class MovieViewHolder extends RecyclerView.ViewHolder
+    {
+        private ImageView imageViewSmallPoster;
+
+        public MovieViewHolder(@NonNull View itemView)
         {
             super(itemView);
+            imageViewSmallPoster = itemView.findViewById(R.id.imageViewSmallPoster);
 
-            textViewContent = itemView.findViewById(R.id.textViewContent);
-            textViewAuthor = itemView.findViewById(R.id.textViewAuthor);
-
+            itemView.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View view)
+                {
+                    if(onPosterClickListener != null)
+                    {
+                        onPosterClickListener.onPosterClick(getAdapterPosition());
+                    }
+                }
+            });
         }
-    }
-
-    public void setReviews(ArrayList<Review> reviews)
-    {
-        this.reviews = reviews;
-
-        ///сообщаем адаптеру ,что данные изменились
-        notifyDataSetChanged();
     }
 }
