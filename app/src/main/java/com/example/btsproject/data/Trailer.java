e508abbf95fbe8fc4934f03a92d3c66baa0b75cc
Dzianis Yauseyenka <dzianis.yauseyenka@unilodz.eu>
2021-01-10 10:33:23 +0100

    In package data added class Trailer

---
+++ app/src/main/java/com/example/btsproject/data/Trailer.java
@@ -0,0 +1,33 @@
+package com.example.btsproject.data;
+
+public class Trailer
+{
+    private String video;
+    private String name;
+
+    public Trailer(String video, String name)
+    {
+        this.video = video;
+        this.name = name;
+    }
+
+    public String getVideo()
+    {
+        return video;
+    }
+
+    public void setVideo(String video)
+    {
+        this.video = video;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+}
