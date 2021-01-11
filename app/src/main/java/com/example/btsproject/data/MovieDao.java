b35c8ddbfcce6ffb6532bfa7f1c4096e4edd8fc4
Dzianis Yauseyenka <dzianis.yauseyenka@unilodz.eu>
2021-01-11 11:18:34 +0100

    In package data added interface MovieDao who will help us to work with data and methods with sql dialect

---
+++ app/src/main/java/com/example/btsproject/data/MovieDao.java
@@ -0,0 +1,44 @@
+package com.example.btsproject.data;
+
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface MovieDao
+{
+    @Query("SELECT * FROM movies")
+    LiveData<List<Movie>> getAllMovies();
+
+    @Query("SELECT * FROM movies WHERE id == :movieId ")
+    Movie getMovieById(int movieId);
+
+    @Query("DELETE FROM movies")
+    void deleteAllMovies();
+
+    @Insert
+    void insertMovie(Movie movie);
+
+    @Delete
+    void deleteMovie(Movie movie);
+
+    ///работа с избронными фильмами
+    @Query("SELECT * FROM favourite_movies")
+    LiveData<List<FavouriteMovie>> getAllMoviesFromFavourites();
+
+    @Query("SELECT * FROM favourite_movies WHERE id == :movieId ")
+    FavouriteMovie getFavouriteMovieById(int movieId);
+
+    @Insert
+    void insertMovieToFavourite(FavouriteMovie movie);
+
+    @Delete
+    void deleteMovieFromFavourite(FavouriteMovie movie);
+
+
+}
