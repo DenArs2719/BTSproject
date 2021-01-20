e2cd25388156ed7cc0b1407b02aa2b6ee6fac490
VetalosUA-KR <vetal925@gmail.com>
2021-01-20 12:28:08 +0200

    update work with JSON, delete Util's classes, and added MovieApiResponse \ RetrofitInstance, to work with Retrofit

---
+++ app/src/main/java/com/example/btsproject/service/MovieApiService.java
@@ -0,0 +1,23 @@
+package com.example.btsproject.service;
+
+import com.example.btsproject.model.MovieApiResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface MovieApiService {
+
+    @GET("movie/popular")
+    Call<MovieApiResponse> getPopularMovies(
+            @Query("api_key") String apiKey,
+            @Query("page") long page);
+
+    @GET("movie/top_rated")
+    Call<MovieApiResponse> getTopRatedMovies(
+            @Query("api_key") String apiKey,
+            @Query("page") long page);
+
+
+
+}
